7834b95fb482881b26cce5815bc53b5fed46e71c
czuniga <czuniga@tibco-support.com>
2020-05-16 18:57:53 -0500

    Agregar interfaz principal

--- miproyectofrontend/src/App.js
+++ miproyectofrontend/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState("");
+  const [pokemonImagenUrl, setPokemonImagenUrl] = useState("");
+  const [yaLeDioClic, setYaLeDioClic] = useState(false);
+  const [informacion, setInformacion] = useState("");
+
+  const darClic = async () => {
+
+    setYaLeDioClic(true);
+
+    const obtenerPokemon = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+    );
+
+    // obtenerPokemon.json().then((data) => {console.log(data);})
+
+    const data = await obtenerPokemon.json();
+
+    setPokemonImagenUrl(data.sprites.front_default);
+    setInformacion(data.abilities[0].ability.name);
+
+    console.log(data);
+
+  };
+
+  const cambioInput = (e) => {
+    setPokemon(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Pokemon Catcher</h1>
+        <input value={pokemon} onChange={(e) => cambioInput(e)} />
+        <button onClick={darClic}>Catch pokemon</button>
+        {yaLeDioClic && <img src={pokemonImagenUrl} alt={"pokemon image"} />}
+        <div>
+          {informacion}
+        </div>
+      </div>
     </div>
   );
 }
